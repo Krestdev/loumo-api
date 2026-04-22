d41bc59671b98535db26c0c5529604e2ed86d2c1
wintercode <kenfackjordanjunior@gmail.com>
2026-04-22 08:39:28 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -73,9 +73,9 @@ export class Server {
     this.app.use(helmet());
     this.app.use(
       cors({
-        // origin: "*", // your frontend's origin
-        origin: ["https://loumoshop.com", "*"], // your frontend's origin
-        credentials: true, // if you use cookies/auth headers
+        origin: ["https://loumoshop.com", "https://admin.loumoshop.com"], // your frontend's origin
+        // origin: ["https://loumoshop.com", "*"], // your frontend's origin
+        // credentials: true, // if you use cookies/auth headers
       }),
     );
 
